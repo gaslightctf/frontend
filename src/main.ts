d7089fc6018de5efb0047ca6d2b96c7ec3444bde
NoRelect <info@norelect.ch>
2025-01-06 19:44:06 +0000

    fix: Add dynamic authorization policy that also hides the metadata endpoint if anonymous access is disabled.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /// <reference types="@angular/localize" />
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpErrorResponse, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor, provideAuth } from 'angular-auth-oidc-client';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
@@ -11,7 +11,7 @@ import { provideEchartsCore } from 'ngx-echarts';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom, inject, provideAppInitializer } from '@angular/core';
 import { DataService } from './app/services/data.service';
-import { mergeMap, NEVER, take } from 'rxjs';
+import { catchError, mergeMap, NEVER, take } from 'rxjs';
 import * as echarts from 'echarts/core';
 import { BarChart, LineChart } from 'echarts/charts';
 import { GridComponent, LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components';
@@ -46,9 +46,20 @@ bootstrapApplication(AppComponent, {
         provideAnimations(),
         provideAppInitializer(() => {
             let dataService = inject(DataService);
-            return dataService.refreshMetadata().pipe(
-                mergeMap(metadata => {
-                    return dataService.loginEvents.pipe(take(1), mergeMap(loginResponse => {
+            return dataService.loginEvents.pipe(take(1), mergeMap(loginResponse => {
+                return dataService.refreshMetadata().pipe(
+                    catchError((err, _) => {
+                        let httpError = err as HttpErrorResponse;
+                        if (httpError.status == 401) {
+                            console.log("Redirecting to authorization endpoint since metadata request got error status 401.");
+                            dataService.login();
+                        } else {
+                            console.error("Failed to fetch metadata.");
+                            console.error(err);
+                        }
+                        return NEVER; // Stall execution since login() triggers a redrect that forces a page reload anyways.
+                    }),
+                    mergeMap(metadata => {
                         if (!metadata.allowAnonymousAccess && !loginResponse.isAuthenticated) {
                             console.log("Redirecting to authorization endpoint since anonymous access is disabled and the player is not logged in.");
                             dataService.login();
@@ -62,8 +73,9 @@ bootstrapApplication(AppComponent, {
                             }
                             return dataService.refreshPages();
                         }
-                    }));
-                }));
+                    })
+                );
+            }));
         }),
     ]
 }).catch(err => console.error(err));
